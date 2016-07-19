2b5109f44fb5e5a8353d54a75b6bb5d54e0182ba
tony un <tony un>
2016-07-19 15:12:23 -0400

    require 'sinatra/test' on test file giving loaderror.

--- github.rb
+++ github.rb
@@ -1,2 +1,18 @@
-require 'bundle/install'
-require ''
+require "bundler/setup"
+require "sinatra/base"
+require "pry"
+
+
+class GitHub < Sinatra::Base
+
+  get "/" do
+    "Welcome"
+  end
+
+
+
+
+
+
+  run! if app_file == $PROGRAM_NAME
+end
